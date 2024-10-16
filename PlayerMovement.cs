cb88a97aaf4472279bd18d611623e0eb30d1ba6f
Fabian Keller <fabian.keller-li@bluewin.ch>
2024-10-17 00:02:19 +0200

    Wall jump fertig

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] public float speedX = 400;
+    [SerializeField] public float speedX = 800;
     [SerializeField] public float speedY = 15;
     private Animator animator;
     public float gravityIncrease = -5;
     Vector2 move;
     private BoxCollider2D boxCollider;
     public float RaycastBoxDistanceDown = 0.2f;
+    
+    //Layers
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
+    
+    //Wallslide
     private bool isWallSliding;
     public float wallSlidingSpeed = 0.2f;
+    
+    //Walljump
+    private bool isWallJumping = false;
+    private float wallJumpingDirection;
+    private float wallJumpingDuration = 0.1f;
+    private float wallJupmingTime = 0.2f;
+    private float wallJumpingCounter;
+    private Vector2 wallJumpingPower = new Vector2(20, 15);
+
 
 
 
@@ -29,12 +42,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(move.x * speedX * Time.deltaTime, rb.velocity.y);
+        if (!isWallJumping)
+        {
+            rb.velocity = new Vector2(move.x * speedX * Time.deltaTime, rb.velocity.y);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(-rb.transform.localScale.x);
+
+        
+
         float HorizontalInput = Input.GetAxisRaw("Horizontal");
 
         move = new Vector2(HorizontalInput, Input.GetAxisRaw("Vertical"));
@@ -47,17 +68,31 @@ public class PlayerMovement : MonoBehaviour
 
         //jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
             Jump();
+        }
+          
 
         //Set animator parameters
         animator.SetBool("flying", HorizontalInput != 0); 
         animator.SetBool("grounded", isGrounded());
 
+
         //Fallbeschleunigung, falls Space nicht mehr gedrückt wird
-        if (isGrounded() == false && !Input.GetKey(KeyCode.Space))
-            rb.AddForce(new Vector2(0, gravityIncrease));
+        if (isGrounded() == false && !Input.GetKey(KeyCode.Space) && !onWall() && !isWallJumping)
+        {
+            rb.AddForce(new Vector2(0 , gravityIncrease));
+        }
 
         wallSlide();
+
+        //Walljump
+        if (Input.GetKeyDown(KeyCode.Space) && onWall() && !isGrounded())
+        {
+            wallJump();
+        }
+
+
     }
 
 
@@ -89,7 +124,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void wallSlide()
     {
-        if (onWall() && !isGrounded())
+        if (onWall() && !isGrounded() && Input.GetAxisRaw("Horizontal") == Mathf.Sign(transform.localScale.x))
         {
             isWallSliding = true;
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
@@ -100,5 +135,34 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    
+    private void wallJump()
+    {
+        if (onWall())
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -Mathf.Sign(transform.localScale.x);
+            wallJumpingCounter = wallJupmingTime;
 
+            CancelInvoke(nameof(stopWallJump));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0;
+        }
+
+        Invoke(nameof(stopWallJump), wallJumpingDuration);
+    }
+
+    private void stopWallJump()
+    {
+        isWallJumping = false;
+    }
 }
